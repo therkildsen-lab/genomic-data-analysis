601ec5ced0ad3866bb3561dab17ff6851815fce1
Nicolas Lou <nicolas931010@hotmail.com>
2019-06-15 18:45:59 -0400

    debugging

--- scripts/subset_beagle_by_lg.sh
+++ scripts/subset_beagle_by_lg.sh
@@ -2,12 +2,12 @@
 ## This script is used to subset a genome-wide beagle file into smaller files by linkage groups or chromosomes. 
 
 BEAGLE=$1 # This should be a beagle.gz file. An example is /workdir/cod/greenland-cod/angsd/bam_list_realigned_mindp161_maxdp768_minind97_minq20.beagle.gz
-LG=$2 # This should be a list of LGs or chromosomes that you want to subset by. An example is /workdir/cod/greenland-cod/sample_lists/lg_list.txt
+LGLIST=$2 # This should be a list of LGs or chromosomes that you want to subset by. An example is /workdir/cod/greenland-cod/sample_lists/lg_list.txt
 
 PREFIX=`echo $BEAGLE | sed 's/\..*//' | sed -e 's#.*/\(\)#\1#'`
 BEAGLEDIR=`echo $BEAGLE | sed 's:/[^/]*$::' | awk '$1=$1"/"'`
 
-for LG in `cat $LG`; do
+for LG in `cat $LGLIST`; do
 	if [ ! -s $BEAGLEDIR$PREFIX"_"$LG".beagle.gz" ]; then
 		echo "Subsetting "$LG
 		zcat $BEAGLE | head -n 1 > $BEAGLEDIR$PREFIX"_"$LG".beagle"
@@ -19,7 +19,7 @@ done
 
 wait 
 
-for LG in `cat $LG`; do
+for LG in `cat $LGLIST`; do
 	if [ ! -s $BEAGLEDIR$PREFIX"_"$LG".beagle.gz" ]; then
 		echo "Gzipping "$LG
 		gzip $BEAGLEDIR$PREFIX"_"$LG".beagle" &
