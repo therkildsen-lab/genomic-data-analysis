951cd635ee0e95ae0f6c8fee2dbc24be92c935ab
Nicolas Lou <nicolas931010@hotmail.com>
2019-06-18 02:14:56 -0400

    Update local_pca_1.sh

--- scripts/local_pca_1.sh
+++ scripts/local_pca_1.sh
@@ -5,7 +5,7 @@ PC=$4
 SNP=$5
 
 for INPUT in `ls $BEAGLEDIR"local_pca/"$PREFIX"_"$LG".beagle.x"*".gz"`; do
-	python /workdir/programs/pcangsd/pcangsd.py -beagle $INPUT -o $INPUT
+	python /workdir/programs/pcangsd/pcangsd.py -beagle $INPUT -o $INPUT -threads 1
 	Rscript --vanilla /workdir/genomic-data-analysis/scripts/local_pca_2.R $INPUT".cov.npy" $PC $SNP $INPUT $LG $BEAGLEDIR"local_pca/"
 	# rm $BEAGLEDIR"local_pca/"$LG".cov.npy"
 	# rm $INPUT
