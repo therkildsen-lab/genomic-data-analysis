7031d4a957bbb9c83730c6fb27ab0806c0b106db
Nicolas Lou <nicolas931010@hotmail.com>
2019-06-18 16:12:40 -0400

    added comments

--- scripts/local_pca_2.R
+++ scripts/local_pca_2.R
@@ -1,4 +1,7 @@
 #!/usr/bin/env Rscript
+## This script is the second dependency of /workdir/genomic-data-analysis/scripts
+## It performs eigen decomposition on a covarianc matrix outputted by pcangsd
+
 args = commandArgs(trailingOnly=TRUE)
 
 cov_matrix <- args[1]
@@ -15,21 +18,26 @@ out_dir <- args[6]
 # lg <- "LG01"
 # out_dir <- "/local/workdir/cod/greenland-cod/angsd/local_pca/"
 
+## Load required libraries
 library(data.table)
 library(lostruct)
 suppressWarnings(suppressMessages(library(tidyverse)))
 library(RcppCNPy)
 
+## Read covariance matrix and perform eigen decomposition
 c <- npyLoad(cov_matrix) %>%
 	as.matrix()
 e <- eigen(c)
 e_values <- e$values
 e_vectors <- as.data.frame(e$vectors)
+
+## Format the output as required by local_pca 
 pca_summary <- c(sum(c^2), e_values[1:pc], unlist(e_vectors[1:pc], use.names=FALSE)) %>%
 	matrix(nrow = 1) %>%
 	as.data.frame()
 write_tsv(pca_summary, paste0(out_dir, "pca_summary_", snp, "snp_", lg, "_", pc, "pc.tsv"), append=T, col_names=F)
 
+## Save the start and end position of each window
 suppressWarnings(suppressMessages(b <- read_tsv(beagle)))
 snp_position <- c(b[[1,1]], b[[dim(b)[1],1]]) %>%
 	matrix(nrow = 1) %>%
