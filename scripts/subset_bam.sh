e18433d364c8c413caf881834c17f5183eca8daa
Nicolas Lou <nicolas931010@hotmail.com>
2019-06-25 00:09:26 -0400

    Create subset_bam.sh
    This script is used to subset bam files based on an identifier (e.g. mitochondrial genome)

---
+++ scripts/subset_bam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## This script is used to subset bam files based on an identifier (e.g. mitochondrial genome)
+
+BAMLIST=$1 # Path to textfile listing bamfiles to subset (e.g. /workdir/cod/greenland-cod/sample_lists/bam_list_realigned.tsv)
+IDENTIFIER=$2 # A feature in bam files to subset for (e.g. MT_genome)
+SUFFIX=$3 # suffix of output (e.g. mtgenome)
+
+for LINE in `cat $BAMLIST`; do
+	NAME=`echo "${LINE%.*}"`
+	echo $NAME
+	samtools view -h $NAME'.bam' | grep $IDENTIFIER > $NAME'_'$SUFFIX'.sam'
+	samtools view -b $NAME'_'$SUFFIX'.sam' > $NAME'_'$SUFFIX'.bam'
+	rm $NAME'_'$SUFFIX'.sam'
+done
