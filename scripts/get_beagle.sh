2e11e5e87fc1f9c3e240ca287b82949d11239f6c
Nicolas Lou <nicolas931010@hotmail.com>
2020-02-26 12:50:00 -0500

    Update get_beagle.sh

--- scripts/get_beagle.sh
+++ scripts/get_beagle.sh
@@ -8,6 +8,18 @@ SNPLIST=$4 # Path to the SNP list
 
 ## Build base name of output files
 OUTBASE=`echo $SNPLIST | sed 's/\..*//' | sed -e 's#.*snp_list_\(\)#\1#'` 
+CHRLIST=`echo $SNPLIST | sed 's/\..*//'`.chrs
 
 ## Get beagle formatted genotype likelihood
-/workdir/Programs/angsd/angsd -b $BAMLIST -anc $REFERENCE -out $BASEDIR'angsd/'$OUTBASE -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 3 -P 32 -sites $SNPLIST >& $BASEDIR'nohups/'$OUTBASE'_get_beagle.log' 
+/workdir/Programs/angsd/angsd \
+-b $BAMLIST \
+-anc $REFERENCE \
+-out $BASEDIR'angsd/'$OUTBASE \
+-GL 1 \
+-doGlf 2 \
+-doMaf 1 \
+-doMajorMinor 3 \
+-P 32 \
+-sites $SNPLIST \
+-rf $CHRLIST \
+>& $BASEDIR'nohups/'$OUTBASE'_get_beagle.log' 
