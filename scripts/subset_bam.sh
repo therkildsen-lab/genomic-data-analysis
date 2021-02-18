df4edf18e2144f48ee625f70240c6df502eb4a43
Nicolas Lou <nicolas931010@hotmail.com>
2021-02-17 23:56:11 -0500

    enabled parallel jobs

--- scripts/subset_bam.sh
+++ scripts/subset_bam.sh
@@ -4,14 +4,31 @@
 BAMLIST=$1 # Path to textfile listing bamfiles to subset (e.g. /workdir/cod/greenland-cod/sample_lists/bam_list_realigned.tsv)
 IDENTIFIER=$2 # A feature in bam files to subset for (e.g. MT_genome)
 SUFFIX=$3 # suffix of output (e.g. mtgenome)
+JOBS=${4:-1} # Number of jobs to run in parallel (default 1)
 
+JOB_INDEX=0
+
+## subset, sort, and convert to bam format
 for LINE in `cat $BAMLIST`; do
 	NAME=`echo "${LINE%.*}"`
-	echo $NAME
-	samtools view -h $NAME'.bam' | head -n 1 > $NAME'_'$SUFFIX'.sam'
-	samtools view -h $NAME'.bam' | grep $IDENTIFIER > $NAME'_'$SUFFIX'.sam'
-	samtools view -b $NAME'_'$SUFFIX'.sam' > $NAME'_'$SUFFIX'.bam'
-	rm $NAME'_'$SUFFIX'.sam'
-	samtools sort $NAME'_'$SUFFIX'.bam' > $NAME'_'$SUFFIX'_sorted.bam'
-	samtools index $NAME'_'$SUFFIX'_sorted.bam'
+	samtools view -h $NAME'.bam' | grep $IDENTIFIER | samtools sort | samtools view -b > $NAME'_'$SUFFIX'_sorted.bam' &
+	JOB_INDEX=$(( JOB_INDEX + 1 ))
+	if [ $JOB_INDEX == $JOBS ]; then
+		wait
+		JOB_INDEX=0
+	fi
+done
+
+wait 
+JOB_INDEX=0
+
+## index bam files
+for LINE in `cat $BAMLIST`; do
+	NAME=`echo "${LINE%.*}"`
+	samtools index $NAME'_'$SUFFIX'_sorted.bam' &
+	JOB_INDEX=$(( JOB_INDEX + 1 ))
+	if [ $JOB_INDEX == $JOBS ]; then
+		wait
+		JOB_INDEX=0
+	fi
 done
