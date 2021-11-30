62ca0bd005781b48cefbe140b41453ba261e003c
nicolas931010 <nicolas931010@hotmail.com>
2021-11-29 19:56:55 -0500

    added a new variable to allow for extra arguments when running ANGSD

--- scripts/angsd_global_snp_calling.sh
+++ scripts/angsd_global_snp_calling.sh
@@ -12,18 +12,25 @@ MINMAF=$8 # Minimum minor allele frequency filter
 MINMAPQ=${9:-20} # Minimum mapping quality (alignment score) filter, default value is 20
 ANGSD=${10:-/workdir/programs/angsd0.931/angsd/angsd} # Path to ANGSD, default value is /workdir/programs/angsd0.931/angsd/angsd
 THREADS=${11:-8} # Number of parallel threads to use, default value is 8.
-
+EXTRA_ARG=${12:-'-remove_bads 1 -only_proper_pairs 1 -C 50'} # Extra arguments when running ANGSD, default value is '-remove_bads 1 -only_proper_pairs 1 -C 50'
 
 ## Extract the name of the bam list (excluding path and suffix)
-BAMLISTNAME=`echo $BAMLIST | sed 's/\..*//' | sed -e 's#.*/\(\)#\1#'` 
+BAMLISTNAME=`echo $BAMLIST | sed 's/\..*//' | sed -e 's#.*/\(\)#\1#'`
 
 ## Build base name of output files
 OUTBASE=$BAMLISTNAME'_mindp'$MINDP'_maxdp'$MAXDP'_minind'$MININD'_minq'$MINQ
 
 ## Call SNPs
-$ANGSD -b $BAMLIST -anc $REFERENCE -out $BASEDIR'angsd/'$OUTBASE -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 1 -doCounts 1 -doDepth 1 -dumpCounts 1 -doIBS 1 -makematrix 1 -doCov 1 -P $THREADS -SNP_pval 1e-6 -setMinDepth $MINDP -setMaxDepth $MAXDP -minInd $MININD -minQ $MINQ -minMaf $MINMAF -minMapQ $MINMAPQ >& $BASEDIR'nohups/'$OUTBASE'.log' 
+$ANGSD -b $BAMLIST -ref $REFERENCE -out $BASEDIR'angsd/'$OUTBASE \
+-GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 1 -doCounts 1 -doDepth 1 -dumpCounts 1 -doIBS 1 -makematrix 1 -doCov 1 \
+-setMinDepth $MINDP -setMaxDepth $MAXDP -minInd $MININD \
+-minQ $MINQ -minMapQ $MINMAPQ \
+-SNP_pval 1e-6 -minMaf $MINMAF \
+-P $THREADS \
+$EXTRA_ARG \
+>& $BASEDIR'nohups/'$OUTBASE'.log'
 
-## Create a SNP list to use in downstream analyses 
+## Create a SNP list to use in downstream analyses
 gunzip -c $BASEDIR'angsd/'$OUTBASE'.mafs.gz' | cut -f 1,2,3,4 | tail -n +2 > $BASEDIR'angsd/global_snp_list_'$OUTBASE'.txt'
 $ANGSD sites index $BASEDIR'angsd/global_snp_list_'$OUTBASE'.txt'
 
